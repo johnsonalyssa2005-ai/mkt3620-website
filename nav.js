6dc950794dffe2cbcbb777aadc76609ec3daf8af
johnsonalyssa2005-ai <johnsonalyssa2005@gmail.com>
2026-09-15 13:06:50 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,37 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    const nav = document.createElement("nav");
-
-    nav.innerHTML = `
-        <div class="nav-container">
-
-            <a href="index.html" class="nav-name">
-                Alyssa Johnson
-            </a>
-
-            <div class="nav-links">
-
-                <a href="index.html">
-                    Product
-                </a>
-
-                <a href="research.html">
-                    Research
-                </a>
-
-                <a href="teaching.html">
-                    Teaching
-                </a>
-
-                <a href="personal.html">
-                    Personal
-                </a>
-
-            </div>
-
-        </div>
-    `;
-
-    document.body.prepend(nav);
-
-});
+    // Navigation bar HTML with interactive dropdowns
+    const navHTML = `
